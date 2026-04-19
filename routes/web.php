69ae65ca8b9bed843173ff1136c4911314ef24cd
Alin Cardenas <alincardenas13@gmail.com>
2026-04-18 19:59:17 -0600

    Diseño vista show y elaboracion de ruta y controlador para cursos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/course', function(){
-    return view('lms.course.create');
-});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -20,5 +19,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('course', CourseController::class);
 
 require __DIR__.'/auth.php';
