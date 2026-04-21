302deaf1f54eb14b9329dd87a3b8dcf31ce78e8d
Alin Cardenas <alincardenas13@gmail.com>
2026-04-20 20:42:24 -0600

    Vistas y contorlador de tareas

---
+++ resources/views/lms/assigment/edit.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between items-center w-full">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Editar material') }}
+            </h2>
+        </div>
+    </x-slot>
+    
+    <div class="py-2">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 shadow-md rounded-2xl p-6">
+                <form method="POST" action="" enctype="multipart/form-data">
+                    @csrf
+                    @include('lms.assigment.form')
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
